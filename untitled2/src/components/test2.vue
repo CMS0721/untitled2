5976aefec72993fb48384183312d59f2882910d6
mingsheng.cui <mingsheng.cui@timanetworks.com>
2020-02-10 18:58:10 +0800

    33333

---
+++ untitled2/src/components/test2.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>sadasdasdasasdads</div>
+</template>
+
+<script>
+    export default {
+        name: "test2",
+        data(){
+            return {
+                sjs:"1231"
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ untitled2/src/components/ysys.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="ssss"></div>
+</template>
+
+<script>
+    export default {
+        name: "ysys",
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
